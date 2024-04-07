2c9493cc507ccbd2b43cdb0075562b5f7eedfb71
wayneleon1 <rurangwaleon10@gmail.com>
2024-04-07 17:48:23 +0200

    setting up font size for headings

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ function App() {
   return (
     <div>
       <h1>Hello world</h1>
+      <h2>Hello world</h2>
+      <h3>Hello world</h3>
+      <h4>Hello world</h4>
+      <p>Texting</p>
     </div>
   );
 }
-
+a;
 export default App;
