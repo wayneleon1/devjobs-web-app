358970f76de6bbb8d4978cd9ba3b39cdf7613755
wayneleon1 <rurangwaleon10@gmail.com>
2024-04-08 21:52:42 +0200

    create header component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-
+// import JobCard from "../components/JobCard";
 function Home() {
-  return <div>Home page</div>;
+  return <div className="mt-20">Home Page</div>;
 }
 
 export default Home;
