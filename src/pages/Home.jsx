6ffbb42ededdc5e6febee80d6d441a44a7e34446
wayneleon1 <rurangwaleon10@gmail.com>
2024-04-08 23:38:36 +0200

    working on inputs container

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-// import JobCard from "../components/JobCard";
+import Input from "../components/UI/HsInput";
 function Home() {
-  return <div className="mt-20">Home Page</div>;
+  return (
+    <div className="grid grid-cols-4 gap-[2px] absolute ">
+      <div className="col-span-2">
+        <Input placeholder="Filter by title, companies, expertise…" />
+      </div>
+      <div className=" col-span-1">
+        <Input placeholder="Filter by location…" />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
